fcf976c6986365c5a6fbebdf85db493f8b244b4f
root <luaregawi@gmail.com>
2023-06-15 04:34:42 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,15 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle"
- * return 0.
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: Always 0.
  */
+
 
 int main(void)
 
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return(0);
 }

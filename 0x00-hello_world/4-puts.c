ca82cf2209a0d510407149965ebbeaad9cd78d26
root <luaregawi@gmail.com>
2023-06-15 04:21:42 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point
- * 
- * Retrun: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return 0.
  */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 
+	return(0);
 }
-

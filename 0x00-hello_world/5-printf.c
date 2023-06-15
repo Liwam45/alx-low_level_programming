8388b24a8fe8277a06a9a5937904fc233d855ba2
root <luaregawi@gmail.com>
2023-06-15 11:32:21 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "with proper grammar, but the outcome is 
+ * main - Prints"with proper grammar, but the outcome is 
  * a piece of art,"followed newline
  * Return: Always 0.
  *
@@ -13,5 +13,5 @@ int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-       	return (0);
+	return (0)
 }

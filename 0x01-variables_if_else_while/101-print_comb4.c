2f93c3912449b8df60015f1a1dd1e5fa9b5daa65
root <luaregawi@gmail.com>
2023-06-16 03:51:42 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,30 +14,32 @@ int main(void)
 
 {
 
-        int d, p, q;
+	int d, p, q;
 
-        for (d = '0'; d < '9'; d++)
-        {
-
-        for (p = d + 1; p <= '9'; p++)
-        {
-
-	for (q = p + 1; q <= '9'; q++)
+	for (d = '0'; d < '9'; d++)
 	{
-        if ((p != d) != q)
-	{      
-        putchar(d);
-        putchar(p);
-	putchar(q);
-        if (d == '7' && p == '8')
-        continue;
-putchar(',');
-putchar(' ');
-        }
-        }
-        }
+
+		for (p = d + 1; p <= '9'; d++)
+		{
+
+			for (q = p + 1; q <= '9'; p++)
+			{
+				if ((p !=q) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
+					if (d == '7' && p == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+		return(0);
+
+
 }
 

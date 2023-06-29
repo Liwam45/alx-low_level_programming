821dc41ab2082de1b311a87f54fcaf6f1b95be94
root <luaregawi@gmail.com>
2023-06-29 04:50:35 -0700

    task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,7 +7,7 @@
  * Return: void
  */
 
-void prints_buffer(char *b, int size)
+void print_buffer(char *b, int size)
 {
 	int o, j, i;
 
@@ -18,7 +18,7 @@ void prints_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
-	while (0 < size)
+	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);

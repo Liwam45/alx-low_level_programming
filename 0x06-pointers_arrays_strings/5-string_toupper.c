a7395b461f22d7b911ad5a16b681da2ab1c106d8
root <luaregawi@gmail.com>
2023-06-29 09:49:13 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 
 
 /**
- * String_toupper - Changes al  lowercase letters
+ * string_toupper - Changes all lowercase letters
  * of a string to uppercase.
  * @str: The string to be changed.
  * Return: A pointer to the changed string.

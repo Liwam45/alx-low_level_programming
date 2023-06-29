9b4a261b1bbf62ba59d79052f08722efb9381a3c
root <luaregawi@gmail.com>
2023-06-29 10:44:50 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,13 +17,13 @@ void rev_string(char *n)
 		i++;
 	}
 	i--;
-	for (j = 0; j < i; j++, i--
-			{
+		for (j = 0; j < i; j++, i--)
+		{
 			temp = *(n + j);
 			*(n + j) = *(n + i);
 			*(n + j) = temp;
-			}
-			}
+		}
+}
 
 /**
  * infinite_add - add 2 numbers together

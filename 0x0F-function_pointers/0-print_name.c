c8bcd2a6021ff047e52ff1071c15df7c339d821b
root <luaregawi@gmail.com>
2023-07-19 07:40:32 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,9 @@
  * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer of function
+ *
  * Return: nothing
- **/
-
-
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
